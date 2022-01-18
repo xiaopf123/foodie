c5c31a544d8b708ca931e81f37d8e3fc067babfd
xiaopengfei <shichao@wsights.com>
2022-01-18 11:50:32 +0800

    1

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.imooc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public Object hello() {
+        return "NMS";
+    }
+}
